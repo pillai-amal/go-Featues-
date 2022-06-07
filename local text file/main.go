69b7acb3fdb69486718e841db847005e9b7d15d7
pillai_amal <mail.vortextv@gmail.com>
2022-06-07 21:12:02 +0530

    added file creation

--- local text file/main.go
+++ local text file/main.go
@@ -1,12 +1,23 @@
 package main
 
-imprt (
+import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 )
 
 func main() {
-	
+	content := "I am writing this sentence into text file"
+	file, err := os.Create("./textfile.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("Wrote the the filw with %v characters", length)
+	defer file.Close()
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
